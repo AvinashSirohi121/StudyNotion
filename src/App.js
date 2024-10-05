848ead75008439399dbfddda2b44c25ce6bba83e
AvinashSirohi121 <avinashsirohi86@gmail.com>
2024-10-05 23:12:48 +0530

    Reducer and Store Configurations

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Home from "./Pages/Home";
 import Login from "./Pages/Login"
 import SignUp from "./Pages/SignUp"
 import AboutPage from "./Pages/AboutPage";
-
+import NavBar from "./components/common/NavBar"
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <NavBar/>
       <Routes>
         <Route path ="/" element={<Home/>}/>
         <Route path="login"  element={<Login />} />
