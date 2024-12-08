c4e06e5abc8812b321da3e07d53f5bf03e09249e
AvinashSirohi121 <avinashsirohi86@gmail.com>
2024-12-08 22:51:59 +0530

    Created dashboard

--- src/App.js
+++ src/App.js
@@ -9,19 +9,44 @@ import ContactUs from "./Pages/ContactUs";
 import VerifyEmail from "./Pages/VerifyEmail";
 import Dashboard from "./Pages/Dashboard";
 import { useDispatch , useSelector} from "react-redux";
+import { ACCOUNT_TYPE } from "./utils/constants"
 import { useEffect } from "react";
+import MyProfile from "./Pages/Dashboard/MyProfile";
+import Settings from "./Pages/Dashboard/Settings";
+import Instructor from "./Pages/Dashboard/Instructor"
+import MyCourses from "./Pages/Dashboard/MyCourses";
+import AddCourse from "./Pages/Dashboard/AddCourse"
+import EditCourse from "./Pages/Dashboard/EditCourse"
+import EnrolledCourses from "./Pages/Dashboard/EnrolledCourses"
+import Cart from "./Pages/Dashboard/Cart"
+import { setUser } from "./slices/profileSlice";
+import Logout from "./Pages/Logout";
+import Error from "./Pages/Error";
+import PurchaseHistory from "./Pages/Dashboard/PurchaseHistory";
+import ForgotPasword from "./Pages/ForgotPasword";
 function App() {
 
   const navigate = useNavigate();
   const dispatch = useDispatch()
 
   const {token} = useSelector((state)=>state.auth);
+  const { user } = useSelector((state) => state.profile)
+  useEffect(()=>{
+      // if(localStorage.getItem("token")){
+      //   dispatch()
+      // }
+      // console.log("Inside first useEffect of App.js")
+      // console.log("User from redux in App.js =>",user)
+      // console.log("User from localStorage in App.js =>",localStorage.getItem('user'))
+      if(user ==null){
+        if(localStorage.getItem("user")){
+          let user = JSON.parse(localStorage.getItem('user'))
 
-  // useEffect(()=>{
-  //     if(localStorage.getItem("token")){
-  //       dispatch()
-  //     }
-  // })
+          //console.log("User from localStorage =>",user, " Image =>",user?.image)
+          dispatch(setUser({...user,image:user?.image}))
+        }
+      }
+  })
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <NavBar/>
@@ -32,9 +57,37 @@ function App() {
         <Route path="about"  element={<AboutPage/>} />
         <Route path="contact"  element={<ContactUs/>} />
         <Route path="verify-email"  element={<VerifyEmail/>} />
-        <Route path="dashboard"  element={<Dashboard/>} />
-        
+        <Route path="dashboard/logout" element={<Logout/> }/>
+        <Route path="dashboard/forgot-password" element={<ForgotPasword/> }/>
+        {/* <Route path="dashboard"  element={<Dashboard/>} /> */}
 
+        <Route element={   <Dashboard />  }>
+          {/* Route for all users */}
+          <Route path="dashboard/my-profile" element={<MyProfile />} />
+          <Route path="dashboard/Settings" element={<Settings />} />
+          {/* Route only for Instructors */}
+          {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+            <>
+              <Route path="dashboard/instructor" element={<Instructor />} />
+              <Route path="dashboard/my-courses" element={<MyCourses />} />
+              <Route path="dashboard/add-course" element={<AddCourse />} />
+              <Route path="dashboard/edit-course/:courseId" element={<EditCourse />}/> 
+            </>
+          )}
+          {/* Route only for Students */}
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route path="dashboard/enrolled-courses" element={<EnrolledCourses />} />
+              <Route path="dashboard/purchase-history" element={<PurchaseHistory />} />
+              <Route path="dashboard/cart" element={<Cart />} />
+            </>
+          )}
+          {/* <Route path="dashboard/settings" element={<Settings />} /> */}
+        </Route>
+
+
+        {/* 404 Page */}
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import LeftPanal from '../components/common/LeftPanal'
+import { Outlet } from 'react-router-dom'
 
 const Dashboard = () => {
+  const { loading: profileLoading } = useSelector((state) => state.profile)
+  const { loading: authLoading } = useSelector((state) => state.auth)
+
+  if (profileLoading || authLoading) {
+    return (
+      <div className="grid min-h-[calc(100vh-3.5rem)] place-items-center">
+        <div className="spinner"></div>
+      </div>
+    )
+  }
   return (
-    <div className='text-white text=4xl'>Dashboard</div>
+    <div className='relative flex min-h-[calc(100vh-3.5rem)]'>
+      <LeftPanal/>
+      <div className='h-[calc(100vh-3.5rem)] overflow-auto'>
+        <div className='mx-auto w-11/12 max-w-[1000px] py-10'>
+          <Outlet/>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
