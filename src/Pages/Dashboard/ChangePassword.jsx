7197559c46c61ca473b3949c9f868f5252b5f085
AvinashSirohi121 <avinashsirohi86@gmail.com>
2024-12-23 23:56:30 +0530

    Comleted the component

--- src/Pages/Dashboard/ChangePassword.jsx
+++ src/Pages/Dashboard/ChangePassword.jsx
@@ -4,20 +4,26 @@ import { IoEyeOff } from "react-icons/io5";
 import { IoEye } from "react-icons/io5";
 import { AiFillStar } from "react-icons/ai";
 import IconBtn from '../../components/Dashboard/Iconbtn';
+import toast from 'react-hot-toast';
+import { changePassword } from '../../services/operations/authMethods';
+import { useDispatch, useSelector } from 'react-redux';
 
 const ChangePassword = () => {
 
     const [data,setData] =useState({
         oldPassword:"",
-        newPassword:"",
-        confirmNewPassword:""
+        password:"",
+        confirmPassword:""
     })
     
   const {validate,validateAll,setErrors,errors} = useValidation();
+  const {token} = useSelector((state =>state.auth))
+  const dispatch = useDispatch()
 
   const [viewOldPassword,setViewOldPassword] = useState(false);
-  const [viewPassword,setViewPassword] = useState(false);
+  const [password,setViewPassword] = useState(false);
   const [viewConfirmPassword,setViewConfirmPassword] = useState(false);
+  const [changeLoading,setChangeLoading]= useState(false);
 
   const handleChange =(e)=>{
     const {name,value} = e.target;
@@ -42,10 +48,10 @@ const ChangePassword = () => {
       case "oldPassword":
         setViewOldPassword(!viewOldPassword);
         break;
-      case "newPassword":
-        setViewPassword(!viewPassword);
+      case "password":
+        setViewPassword(!password);
         break;
-      case "confirmNewPassword":
+      case "confirmPassword":
           setViewConfirmPassword(!viewConfirmPassword);
           break;
       default:
@@ -54,11 +60,40 @@ const ChangePassword = () => {
     
 }
 
+const changePass =()=>{
+  console.log("Old pass =>",data.oldPassword," New Pass =>",data.password," Confirm New =>",data.confirmPassword)
+  if(data.oldPassword =="" || data.password =="" || data.confirmPassword ==""){
+    toast.error("Kindly fill all the fileds",{duration:3000})
+  }else{
+    if(data.oldPassword === data.password){  
+    toast.error("Old Password and New Password are same",{duration:3000})
+    }else{
+      if(data.password !== data.confirmPassword){
+        toast.error("New Password and Confirm New Password does not match",{duration:3000})
+      }else{
+        setChangeLoading(true);
+        dispatch(changePassword(token,{oldPass:data.oldPassword,newPass:data.password,confirmNewPass:data.confirmPassword}))
+        setData({
+          oldPassword:"",
+          password:"",
+          confirmPassword:""
+      })
+        setChangeLoading(false);
+      }
+    }
+  }
+ 
+  
+}
+
   return (
     <div className='flex-col mt-[3rem] bg-richblack-800 p-[2rem] rounded-lg px-8 flex justify-between  border-[1px] border-richblack-600'>
     <div className='flex w-[100%] justify-between'>
       <p className='font-bold text-lg'>Change Password</p>
-      <IconBtn text="Save"></IconBtn>
+      <IconBtn 
+          text={changeLoading ? "Saving..." : "Save"} 
+          className={`${changeLoading ? "cursor-not-allowed" : ""}`}
+          onclick={()=>changePass()}></IconBtn>
       </div>
 
     <div className='flex flex-col lg:justify-between  gap-2 md:mb-2 sm:mb-2 lg:mb-8 '>
@@ -76,27 +111,27 @@ const ChangePassword = () => {
                             <button 
                                 onClick={()=>togglePassword("oldPassword")}
                                 className="absolute right-2 top-4 ">
-                                {viewPassword ?  <IoEye/> :<IoEyeOff/>}   
+                                {viewOldPassword ?  <IoEye/> :<IoEyeOff/>}   
                             
                             </button>
                         </div>
-                        {errors.password && <span className="text-[10px]   text-pink-1000">{errors.password}</span>}
+                        {/* {errors.password && <span className="text-[10px]   text-pink-1000">{errors.password}</span>} */}
                     </div>
                     <div className='flex flex-col'>
                         <label className='flex text-sm'>New Password <AiFillStar className='text-[5px]  text-pink-1000'/></label>
                         <div className="flex relative">
                             <input 
-                            type={viewPassword ? "text":"password"}
-                            name="newPassword"
-                            value={data.newPassword}
+                            type={password ? "text":"password"}
+                            name="password"
+                            value={data.password}
                             onChange={(e)=>handleChange(e)}
                             className='h-[40px]  w-full  rounded-lg bg-richblack-700 pl-2 border-b border-richblack-400 outline-none mt-1' 
                             placeholder='Enter password'/>
                             
                             <button 
-                                onClick={()=>togglePassword("newPassword")}
+                                onClick={()=>togglePassword("password")}
                                 className="absolute right-2 top-4 ">
-                                {viewPassword ?  <IoEye/> :<IoEyeOff/>}   
+                                {password ?  <IoEye/> :<IoEyeOff/>}   
                             
                             </button>
                         </div>
@@ -107,13 +142,13 @@ const ChangePassword = () => {
                     <div className="flex relative">
                             <input 
                             type={viewConfirmPassword ? "text":"password"}
-                            name="confirmNewPassword"
-                            value={data.confirmNewPassword}
+                            name="confirmPassword"
+                            value={data.confirmPassword}
                             onChange={(e)=>handleChange(e)}
                             className='h-[40px] w-full   rounded-lg bg-richblack-700 pl-2 border-b border-richblack-400 outline-none mt-1' 
                             placeholder='Confirm Password'/>
                             <button 
-                                onClick={()=>togglePassword("confirmNewPassword")}
+                                onClick={()=>togglePassword("confirmPassword")}
                                 className="absolute right-2 top-4 ">
                                 {viewConfirmPassword ?  <IoEye/> :<IoEyeOff/>}   
                             
