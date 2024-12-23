4996c1534eb4a55d9311a658b1629e6bc9b3bc39
AvinashSirohi121 <avinashsirohi86@gmail.com>
2024-12-23 23:56:59 +0530

    Completed component

--- src/Pages/Dashboard/DeleteProfile.jsx
+++ src/Pages/Dashboard/DeleteProfile.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import { RiDeleteBin5Line } from "react-icons/ri";
+import { useDispatch, useSelector } from 'react-redux';
+import { accountDeletion } from '../../services/operations/authMethods';
+
 
 const DeleteProfile = () => {
+  const {token} = useSelector((state)=>state.auth);
+  const dispatch = useDispatch();
+
+  const deleteAccount=()=>{
+      dispatch(accountDeletion(token))
+  }
   return (
     <div className='flex mt-[3rem] bg-pink-900 p-[2rem] rounded-lg px-10 gap-5  border-[1px] border-pink-400'>
        <div className='h-[4rem] bg-pink-800 w-[4rem] flex justify-center items-center p-4 rounded-full'>
@@ -11,7 +20,8 @@ const DeleteProfile = () => {
             <p className='text-2xl text-white'>Delete Account</p>
             <p>Would you linke to delete account ?</p>
             <p>This account may contain Paid Courses. Deleting your account is permanent and will remove all the content accociated with it.</p>
-            <p className='text-pink-200 italic mt-2 hover:text-pink-50 cursor-pointer'>I want to delete my account</p>
+            <p className='text-pink-200 italic mt-2 hover:text-pink-50 cursor-pointer' 
+              onClick={deleteAccount}>I want to delete my account</p>
         </div>
      </div>   
   )
