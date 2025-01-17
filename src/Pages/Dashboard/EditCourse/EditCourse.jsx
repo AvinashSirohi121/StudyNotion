ed4c6765a3e29e055e25475200bf574b79ceb609
AvinashSirohi121 <avinashsirohi86@gmail.com>
2025-01-17 20:59:07 +0530

    Edit CoursePage

---
+++ src/Pages/Dashboard/EditCourse/EditCourse.jsx
@@ -0,0 +1,28 @@
+import React,{useEffect} from 'react'
+import RenderSteps from '../AddCourse/RenderSteps'
+import { useDispatch } from 'react-redux'
+import { useLocation } from 'react-router-dom';
+import { setEditCourse } from '../../../slices/courseSlice';
+
+const EditCourse = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(()=>{
+    if (location.pathname.includes("/dashboard/edit-course")) {
+      dispatch(setEditCourse(true));
+    }
+  },[dispatch, location.pathname])
+  return (
+    <div className="flex flex-1 flex-col">
+      <h1 className="mb-14 text-3xl font-medium text-richblack-5">
+        Edit Course
+      </h1>
+      <div className="flex-1">
+        <RenderSteps />
+      </div>
+    </div>
+  )
+}
+
+export default EditCourse
