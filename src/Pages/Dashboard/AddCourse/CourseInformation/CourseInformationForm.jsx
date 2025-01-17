0b12f8f8b405318b4226cf3e156051c78ca3501d
AvinashSirohi121 <avinashsirohi86@gmail.com>
2025-01-17 21:03:57 +0530

    Changes in editCOurse Initatior method

--- src/Pages/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
+++ src/Pages/Dashboard/AddCourse/CourseInformation/CourseInformationForm.jsx
@@ -11,7 +11,7 @@ import Tags from "../../../../components/Dashboard/Tags"
 
 import Upload from '../../../../components/common/Upload';
 import {setCourse, setCourseCategory,setStep,setEditCourse} from "../../../../slices/courseSlice"
-import { createCourse } from '../../../../services/operations/courseMethods';
+import { createCourse,editCourses } from '../../../../services/operations/courseMethods';
 
 
 const CourseInformationForm = () => {
@@ -31,10 +31,11 @@ const CourseInformationForm = () => {
     const [courseImage,setCourseImage] = useState("");
     const previousPath = useRef(location.pathname);
 
-    //console.log("CourseCategory =>",courseCategory)
+   
 
     const data = useLocation();
     const setEditableCourseData = (data) => {
+      dispatch(setCourse(data));
       //console.log("Inside setEditableCourseData =>", data);
     
       
@@ -42,7 +43,8 @@ const CourseInformationForm = () => {
       setCourseData((prevState) => {
         const categoryId = data?.category[0];
         const categoryName = courseCategory.find((cat) => cat._id === categoryId)?.name || prevState.category;
-      //  console.log("Edited Category =>",categoryName)
+         //console.log("data",data)
+        // console.log("data?.courseImage =>",data?.thumbNail," prevState.courseImage =>",prevState.courseImage, prevState.thumbNail )
       return {
         ...prevState,
         courseName: data?.courseName || prevState.courseName,
@@ -54,7 +56,7 @@ const CourseInformationForm = () => {
         status: data?.status || prevState.status,
         instructor: data?.instructor || prevState.instructor,
         instructions: data?.instructions || prevState.instructions,
-        courseImage: data?.courseImage || prevState.courseImage,
+        courseImage: data?.thumbNail || prevState.courseImage,
       }
     });
       setInstructions(data?.instructions)
@@ -134,71 +136,152 @@ const CourseInformationForm = () => {
     
   }
 
-  const initiateCourse =async()=>{
-    console.log("Edit Course =>",editCourse)
-    // if(editCourse){
-    //   console.log("Calling befor Saving edited course");
-    //   console.log("CourseData =>",courseData);
-    // }
-    // else{ 
-      if (courseData.courseName !=="" &&
-      courseData.courseDescription !=="" &&
-      courseData.price !=="" &&
-      courseData.courseImage !=="" &&
-      courseData.courseCategories !=="" &&
-      courseData.tag.length >0 &&
-      courseData.instructions.length>0 &&
-      courseData.whatYouWillLearn !=="" &&
-      courseData.instructor !==""
+  const  checkIfCourseEdited=()=>{
+
+    if(courseData?.courseName !== course?.courseName ||
+      courseData?.courseDescription !== course?.courseDescription ||
+      courseData?.price !== course?.price ||
+      courseData.courseCategory !== course?.courseCategory ||
+      courseData.tag !== course?.tag ||
+      courseData.instructions !== course?.instructions || 
+      courseData.whatYouWillLearn !== course?.whatYouWillLearn ||
+      courseData.courseImage !== course?.courseImage
     ){
-      console.log("CoursImage=>",courseData.courseImage)
-      const formData = new FormData();
-      
-     
-      formData.append('courseName',courseData?.courseName)
-      formData.append('courseDescription',courseData?.courseDescription)
-      formData.append('courseImage',courseData?.courseImage);
-      formData.append('whatYouWillLearn',courseData?.whatYouWillLearn)
-      formData.append('price',courseData?.price)
-      formData.append('tag',JSON.stringify(courseData?.tag))
-      formData.append('category',courseData?.category)
-      formData.append('status',courseData?.status)
-      formData.append('instructor',courseData?.instructor)
-      formData.append('instructions',JSON.stringify(courseData?.instructions))
-      
+      return true
+    }
+    return false
+  }
+  const initiateCourse =async()=>{
+    // console.log("Edit Course =>",editCourse," Step =>",step)
+     console.log("Course from Redux =>",course)
+    let formData = new FormData();
+    if(editCourse){
+      // console.log("Calling befor Saving edited course");
+      // console.log("CourseData =>",courseData);
+      let IfCourseEdited = checkIfCourseEdited();
+      console.log("Is Course Edited =>",IfCourseEdited);
+
+      if(courseData.courseName !=="" && courseData.courseName !== course?.courseName){
+        formData.append("courseName",courseData.courseName)
+        console.log("OldCourseName =>",course?.courseName," UpdatedCourseName =>",courseData.courseName)
+      }
+      if(courseData.courseDescription !=="" && courseData.courseDescription !== course?.courseDescription){
+        formData.append("courseDescription",courseData.courseDescription)
+        console.log("OldcourseDescription =>",course?.courseDescription," UpdatedcourseDescription =>",courseData.courseDescription)
+      }
+      if(courseData.price !=="" && courseData.price !== course?.price){
+        formData.append("price",courseData.price)
+        console.log("OldCourseName =>",course?.price," UpdatedCourseName =>",courseData.price)
+      }
+      if(courseData.courseImage !=="" && courseData.courseImage !== course?.thumbNail){
+        formData.append('courseImage',courseData?.courseImage);
+        console.log("OldCourseImage =>",course?.thumbNail," UpdatedCourseImage =>",courseData.courseImage)
+      }
+      if(courseData.whatYouWillLearn !=="" && courseData.whatYouWillLearn !== course?.whatYouWillLearn){
+        formData.append('whatYouWillLearn',courseData?.whatYouWillLearn)
+        console.log("Old WhatYouWillLearn=>",course?.whatYouWillLearn," Updated WhatYouWillLearn =>",courseData.whatYouWillLearn)
+      }
+      if(courseData.tag.length > 0 && courseData.tag.toString() !== course?.tag.toString()){
+        formData.append('tag',JSON.stringify(courseData?.tag))
+        console.log("Old tags =>",course?.tag," Updated Tags=>",courseData.tag)
+      }
+      if(courseData.category !=="" && courseData.category !== course?.category[0]){
+        formData.append('category',courseData?.category)
+        console.log("OldCategory =>",course?.category[0]," UpdatedCategory =>",courseData.category)
+      }
+      if(courseData.instructions.length >0  && courseData.instructions.toString() !== course?.instructions.toString()){
+        formData.append('instructions',JSON.stringify(courseData?.instructions))
+        console.log("Old Instructions =>",course?.instructions," UpdatedInstrctions =>",courseData.instructions)
+      }
+
       for (let [key, value] of formData.entries()) {
         console.log(`${key}: ${value}`);
       }
-      
-    
-      try {
-        const data = await createCourse(formData, token); // Call the API function
-        if (data) {
-          dispatch(setCourse(data));
-          dispatch(setStep(2));
-          console.log("Course Created:", data);
-          if(editCourse==true){
-            dispatch(setEditCourse(false));
+      console.log("Lenght =>",Array.from(formData.entries()).length)
+      if(Array.from(formData.entries()).length>0){
+        try {
+          formData.append('courseId',course?._id);
+          for (let [key, value] of formData.entries()) {
+            console.log(`${key}: ${value}`);
           }
+          const data = await editCourses(token,formData); // Call the API function
+          if (data) {
+            dispatch(setCourse(data));
+            dispatch(setStep(2));
+            console.log("Course Edited:", data);
+            if(editCourse==true){
+              dispatch(setEditCourse(false));
+            }
+            console.log("Printing after editing the course = the course")
+          }
+        } catch (error) {
+          console.error("Failed to edit course:", error);
         }
-      } catch (error) {
-        console.error("Failed to create course:", error);
+     
       }
       
-      
-      
-
-  //  }
-  }
+    }else{
+      // course is created for First Time
+      if (courseData.courseName !=="" &&
+        courseData.courseDescription !=="" &&
+        courseData.price !=="" &&
+        courseData.courseImage !=="" &&
+        courseData.courseCategories !=="" &&
+        courseData.tag.length >0 &&
+        courseData.instructions.length>0 &&
+        courseData.whatYouWillLearn !=="" &&
+        courseData.instructor !==""
+      ){
+        console.log("CoursImage=>",courseData.courseImage)
+       
+        
+       
+        formData.append('courseName',courseData?.courseName)
+        formData.append('courseDescription',courseData?.courseDescription)
+        formData.append('courseImage',courseData?.courseImage);
+        formData.append('whatYouWillLearn',courseData?.whatYouWillLearn)
+        formData.append('price',courseData?.price)
+        formData.append('tag',JSON.stringify(courseData?.tag))
+        formData.append('category',courseData?.category)
+        formData.append('status',courseData?.status)
+        formData.append('instructor',courseData?.instructor)
+        formData.append('instructions',JSON.stringify(courseData?.instructions))
+        
+        try {
+          const data = await createCourse(formData, token); // Call the API function
+          if (data) {
+            dispatch(setCourse(data));
+            dispatch(setStep(2));
+            console.log("Course Created:", data);
+            if(editCourse==true){
+              dispatch(setEditCourse(false));
+            }
+            console.log("Printing after creating the course = the course")
+          }
+        } catch (error) {
+          console.error("Failed to create course:", error);
+        }
+       
+    
+    }
+    
    
-  }
+    }
+
+   
+    
+    
+  
+   
+
+}
 
   useEffect(() => {
     const fetchData = async () => {
         if (courseCategory.length === 0) {
             setLoading(true);
             let category = await fetchCategories();
-            dispatch(setCourseCategory(category.data.data));
+            dispatch(setCourseCategory(category?.data?.data));
             setLoading(false);
         }
     };
@@ -206,6 +289,7 @@ const CourseInformationForm = () => {
     if(data.state){
       setEditableCourseData(data.state);
     }
+    
 
 }, [data]);
 
@@ -268,6 +352,7 @@ const CourseInformationForm = () => {
             </div>
             
             <Tags
+                courseTags={courseData.tag}
                 placeholder="Add your tags here"
                 onTagsChange={handleTagsChange} // Callback to receive the tags
             />
@@ -341,19 +426,20 @@ const CourseInformationForm = () => {
                  </div>
                 {errors.instruction && <span className="text-[10px]  text-pink-1000">{errors.instruction}</span>}
                 <div className="flex-wrap max-w-[80%]">
-                        {instructions.length > 0 &&
-                            instructions.map((i) => (
-                            <div key={i.id} className='flex items-end gap-2 justify-between px-2 mt-2 '>
-                                <p className="max-w-[80%] break-words  italic">
-                                {i?.name}
-                                </p>
-                                <button
-                                    className="text-pink-1000 bg-pink-900 p-1 rounded-2xl text-xs mb-[2px] cursor-pointer"
-                                    onClick={() => deleteInstruction(i.id)}>
-                                        remove
-                                    </button>
-                            </div>
-                            ))}
+                {instructions.map((i, index) => (
+                      <div key={i.id} className="flex items-end gap-2 justify-between px-2 mt-2">
+                        <p className="max-w-[80%] break-words italic">
+                        {`${index + 1}. ${i?.name}`}
+                        </p>
+                        <button
+                          className="text-pink-1000 bg-pink-900 p-1 rounded-2xl text-xs mb-[2px] cursor-pointer"
+                          onClick={() => deleteInstruction(i.id)}
+                        >
+                          remove
+                        </button>
+                      </div>
+                    ))}
+
                         </div>
 
             </div>
@@ -361,10 +447,15 @@ const CourseInformationForm = () => {
              
 
         </div>
-        <div className='mt-4 flex justify-end'>              
+        <div className='mt-4 flex gap-3 justify-end'>  
+            {editCourse ?            
+            <button 
+              onClick={()=>dispatch(setStep(2))}
+              className='h-[35px] px-2 lg:mr-3 flex items-center justify-between  bottom-0 rounded-lg bg-richblack-400 text-white z-10'>Continue Without Saving <MdNavigateNext /> </button>                  
+            :""}
             <button 
               onClick={()=>initiateCourse()}
-              className='w-[15%]  h-[35px]  mr-3 flex items-center justify-center  bottom-0 rounded-lg bg-yellow-50 text-black z-10'>Next <MdNavigateNext /> </button>                  
+              className='h-[35px] px-2 lg:mr-3 flex items-center justify-between  bottom-0 rounded-lg bg-yellow-50 text-black z-10'>{editCourse ? "Save Changes" :"Next"} <MdNavigateNext /> </button>                  
           </div> 
 
        
