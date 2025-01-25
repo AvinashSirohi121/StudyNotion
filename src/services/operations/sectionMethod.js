d1f63396a5bfff0c40f023ddcd24f097dd57e35b
AvinashSirohi121 <avinashsirohi86@gmail.com>
2025-01-25 23:38:45 +0530

    Delete Section Method

--- src/services/operations/sectionMethod.js
+++ src/services/operations/sectionMethod.js
@@ -28,3 +28,31 @@ export const createSection = async(token,data)=>{
     }
 }
   
+
+export const deleteSection = async(token,data)=>{
+
+    let result=[];
+    let toastId = toast.loading("Deleteing Section");
+     try {
+      //console.log("Deleting Section =>",data);
+     
+      let response = await apiConnector("POST",courseEndPoints.DELETE_SECTION_API,data,{
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      });
+  
+            console.log("Deleting Section Response =>",response);
+            result=response?.data?.data;
+            return result;
+     
+      
+    } catch (error) {
+        console.log("Error in deleting section =>",error)
+        toast.error(`${error?.response?.data?.message}`,{duration:3000})
+        
+        throw error
+    }finally{
+      toast.dismiss(toastId);
+    }
+}
+  
